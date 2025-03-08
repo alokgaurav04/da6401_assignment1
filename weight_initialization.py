969bde0b52d0782264910ccd1e8ecdb478af385c
alokgaurav04 <alokgaurav04@gmail.com>
2025-03-08 22:56:44 +0530

    Add files via upload

--- weight_initialization.py
+++ weight_initialization.py
@@ -1,7 +1,13 @@
 import numpy as np
 import math
 
-def rand_initialization(prev_neurons,curr_neurons):
+def random_init(prev_neurons,curr_neurons):
     weights = np.random.randn(prev_neurons,curr_neurons)*0.1
     bias=np.random.randn(1,curr_neurons)
     return weights,bias
+
+def xavier_init(prev_neurons, curr_neurons):
+    lower_limit, upper_limit = -math.sqrt(6.0/(curr_neurons + prev_neurons)), math.sqrt(6.0/(curr_neurons + prev_neurons)) 
+    weights = np.random.uniform(lower_limit, upper_limit, size=(prev_neurons, curr_neurons))
+    bias = np.random.uniform(lower_limit, upper_limit, size=(1, curr_neurons))
+    return weights, bias
