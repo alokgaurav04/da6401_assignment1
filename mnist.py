c7d02fcf66d00aa82e086c6ae04260fc869f0524
alokgaurav04 <alokgaurav04@gmail.com>
2025-03-17 23:49:45 +0530

    Code for MNIST dataset

--- mnist.py
+++ mnist.py
@@ -1,17 +1,29 @@
 from NeuralNetwork import FeedForwardNN,mnist,np
 import matplotlib.pyplot as plt
 from Loss_Function import *
+import wandb
+
+# Fetch the best model from the sweep
+api = wandb.Api()
+sweep = api.sweep(f"DA6401_Assignment_1/kvo0qk0z")
+
+# Sort runs by validation accuracy
+best_run = sorted(sweep.runs, key=lambda run: run.summary.get("val_accuracy", 0), reverse=True)[2]
+
+# Extract best model's hyperparameters
+best_config = best_run.config
+print(f"Best model found: {best_config}")
 
 input_size = 28 * 28  # Flattened image size
 output_size = 10  # Number of classes
 hidden_layers=[]
-no_of_hidden_layer=2
-hidden_layer_size=128
+no_of_hidden_layer=best_config['hidden_layers']
+hidden_layer_size=best_config['hidden_layer_size']
 for i in range(no_of_hidden_layer):
     hidden_layers.append(hidden_layer_size)
 
 # Initialize the neural network
-nn = FeedForwardNN(input_size, hidden_layers, output_size,activation='relu',weight_init='random_init',loss_func='MSE', learn_rate=0.001, grad_desc='adam')
+nn = FeedForwardNN(input_size, hidden_layers, output_size,activation=best_config['activation_func'],weight_init=best_config['weight_init'],loss_func='MSE', learn_rate=best_config['learning_rate'], grad_desc=best_config['optimizer'])
 
 # Load the Fashion MNIST dataset
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
@@ -28,7 +40,7 @@ y_test_one_hot = np.zeros((y_test.shape[0], 10))
 y_test_one_hot[np.arange(y_test.shape[0]), y_test] = 1
 
 # Train the model
-nn.train(X_train, y_train_one_hot,batch_size=40, epochs=5,val_split=0.1)
+nn.train(X_train, y_train_one_hot,batch_size=best_config['batch_size'], epochs=best_config['epochs'],val_split=0.1)
 
 # # Test the model
 y_pred = nn.predict(X_test)
